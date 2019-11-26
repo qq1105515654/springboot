9cfe598f200c8d814af9f3e151469e7a66f091f9
Administrator <1105515654@qq.com>
2019-11-26 17:55:23 +0800

    Es的应用

---
+++ src/main/java/com/extend/demo/search/ElasticSearchClient.java
@@ -0,0 +1,24 @@
+package com.extend.demo.search;
+
+import javafx.scene.NodeBuilder;
+import org.elasticsearch.cluster.metadata.AliasMetaData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @ClassName ElasticSearchClient
+ * @Description TODO
+ * @Author snh
+ * @Date 2019/9/5 16:25
+ * @Version 1.0
+ **/
+@Configuration
+public class ElasticSearchClient {
+
+
+}
